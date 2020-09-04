862094e6aac1a079c61ba5424575d3c4f92e38cc
hellolml <2581154176@qq.com>
2020-09-04 12:54:06 +0800

    在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。

---
+++ Find.java
@@ -0,0 +1,18 @@
+public class Find {
+    public boolean Find(int target, int[][] array) {
+        int rows = array.length;
+        int cols = array[0].length;
+        int row = 0;
+        int col = cols - 1;
+        while (row < rows && col >= 0) {
+            if (array[row][col] == target) {
+                return true;
+            } else if (array[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+}
